36642d88a1e38a0369bcc79f2ffc721eb150167b
Julian <julianete4@gmail.com>
2023-02-24 11:17:52 -0300

    modelo y endpoint modificados

--- models/User.js
+++ models/User.js
@@ -3,7 +3,13 @@ import mongoose, { Schema } from "mongoose";
 let schema = new mongoose.Schema({
     name: {type: String, required: true},
     mail: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    is_online: { type: Boolean },
+    is_admin: { type: Boolean },
+    is_author: { type: Boolean },
+    is_company: { type: Boolean },
+    is_verified: { type: Boolean, required: true },
+    verify_code: { type: String, required: true }
 }, {
     timestamps: true
 })
--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,12 @@ router.get('/', function(req, res, next) {
 
 router.post('/', async (req,res) => {
   try{
+    req.body.is_online = false
+    req.body.is_admin = false
+    req.body.is_author = false
+    req.body.is_company = false
+    req.body.is_verified = false
+    req.body.verify_code = false
     let user = await User.create(req.body)
     return res.status(201).json({
       success: true,
