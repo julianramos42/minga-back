79f4be374468e5ccc0409a395ba5332fab2e3701
JuanBarberis <juanbarberis.97@gmail.com>
2023-03-03 13:47:02 -0300

    fix

--- middlewares/chapthers/next_order.js
+++ middlewares/chapthers/next_order.js
@@ -6,6 +6,7 @@ async function nextOrder(req, res, next) {
         let chapther = await Chapther.find({ manga_id: req.body.manga_id }).sort("-order")  //buscar en todos los chapther con el mismo manga.id, el que tenga el order mayor
         chapther = chapther[0]
         req.body.order = chapther.order + 1
+    console.log(chapther)
     }
     
 
