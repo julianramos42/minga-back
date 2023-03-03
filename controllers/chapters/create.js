22b685f06d82a85e5c3828c6096a4490697b3cc0
JuanBarberis <juanbarberis.97@gmail.com>
2023-03-03 10:12:18 -0300

    fix message controller

--- controllers/chapters/create.js
+++ controllers/chapters/create.js
@@ -16,7 +16,7 @@ const controller = {
             console.log(error)
             return res.status(400).json({
                 success: false,
-                message: 'no se pudo crear',
+                message: 'chapther cannot by create',
 
             })
         }
--- controllers/chapters/read_all.js
+++ controllers/chapters/read_all.js
@@ -1,9 +1,9 @@
 const controller = {
 
-    read_all: (req, res) => { /* funcion que se va a ejecutar cada vez que se llame al endpoint */
-        //console.log(req)
+    read_all: (req, res) => { 
+       
         return res
-            .status(200) /* 200: exito para la lectura */
+            .status(200) 
             .json({
                 success: true,
                 message: 'aca deberias ver todos los capitulos'
