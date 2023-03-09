df0a3a4802864c2c19bcf13876c6bccfce4b58d9
JuanBarberis <juanbarberis.97@gmail.com>
2023-03-09 12:39:38 -0300

    fix

--- controllers/category/all.js
+++ controllers/category/all.js
@@ -5,7 +5,7 @@ const all = {
         async (req, res, next) => {
             try {
                 let all = await category.find()
-                if (all.length === 0) {
+                if (all.length > 0) {
  
                     return res.status(200).json({
                         success: true,
