bd02c666578c8f140e298380fad922b9edf306b1
JuanBarberis <juanbarberis.97@gmail.com>
2023-03-17 12:40:09 -0300

    fix

--- middlewares/authors/chapter_is_property_of.js.js
+++ middlewares/authors/chapter_is_property_of.js.js
@@ -11,18 +11,19 @@ async function chapter_is_property_of(req, res, next) {
         if (chapter) {
             const manga = await Manga.findOne({
                 _id: chapter.manga_id,
+                author_id: req.body.author_id,
             });
             if (manga) {
                 return next();
             }
             return res.status(404).json({
                 succes: false,
-                message: "The manga not found",
+                message: "Manga not found",
             });
         } else {
             return res.status(404).json({
                 succes: false,
-                message: "The chapter not found",
+                message: "Chapter not found",
             });
         }
     } catch (error) {
