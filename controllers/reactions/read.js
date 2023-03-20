b798a516caec1a9451323201f3017d644ffc26f0
Julian <julianete4@gmail.com>
2023-03-20 17:22:46 -0300

    M-17 terminada

--- controllers/reactions/read.js
+++ controllers/reactions/read.js
@@ -1,9 +1,22 @@
 import { Reaction } from '../../models/Reaction.js'
+import { Manga } from '../../models/Manga.js'
 
 const controller = {
     read: async (req, res, next) => {
         try {
-            if (req.query.manga_id && !req.query.user_id) {
+            let order = { title: 1 }
+            if (req.query.order == 1 || req.query.order == -1) {
+                order.title = req.query.order
+            }
+
+            let pagination = { page: 1, limit: 6 }
+            if (req.query.page) {
+                pagination.page = Number(req.query.page)
+            }
+
+            let skip = pagination.page > 1 ? (pagination.page - 1) * pagination.limit : 0
+
+            if (req.query.manga_id && !req.query.me) {
                 let query = {}
                 query.manga_id = req.query.manga_id
 
@@ -22,10 +35,10 @@ const controller = {
                     }
                 })
             }
-            if (req.query.manga_id && req.query.user_id) {
+            if (req.query.manga_id && req.query.me) {
                 let query = {}
                 query.manga_id = req.query.manga_id
-                query.user_id = req.query.user_id
+                query.user_id = req.user._id
 
                 let reactions = await Reaction.find(query)
 
@@ -34,16 +47,32 @@ const controller = {
                     message: reactions
                 })
             }
-            if (req.query.user_id && req.query.name) {
+            if (req.query.name) {
                 let query = {}
-                query.user_id = req.query.user_id
+                query.user_id = req.user._id
                 query.name = req.query.name
 
                 let reactions = await Reaction.find(query)
+                    .select("manga_id -_id")
+                let favouritesMangas = []
+                for(let reaction of reactions){
+                    favouritesMangas.push(reaction.manga_id)
+                }
+                let mangasQuery = {
+                    _id: favouritesMangas
+                }
+                if (req.query.category) {
+                    mangasQuery.category_id = req.query.category.split(',')
+                    pagination.limit = 10
+                }
+                let mangas = await Manga.find(mangasQuery)
+                    .sort(order)
+                    .skip(skip)
+                    .limit(pagination.limit > 0 ? pagination.limit : 0)
 
                 return res.status(200).json({
                     success: true,
-                    message: reactions
+                    message: mangas
                 })
             }
         } catch (error) {
