d0336461b2578255cfd750f29b1999732eb7cd1d
Julian <julianete4@gmail.com>
2023-03-21 11:59:50 -0300

    back terminado

---
+++ controllers/comments/destroy.js
@@ -0,0 +1,19 @@
+import { Comment } from "../../models/Comment.js";
+
+const controller = {
+    destroy: async (req, res, next) => {
+        try {
+            let { id } = req.params
+            await Comment.deleteOne(
+                { _id: id }
+            )
+            return res.status(200).json({
+                message: 'Comment deleted'
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+export default controller;
---
+++ controllers/comments/update.js
@@ -0,0 +1,19 @@
+import { Comment } from "../../models/Comment.js";
+
+const controller = {
+    update: async (req, res, next) => {
+        try {
+            let { id } = req.params
+            let upd = await Comment.findOneAndUpdate(
+                { _id: id },
+                req.body,
+                { new: true },
+            )
+                .select('text')
+            return res.status(200).json(upd)
+        } catch (error) {
+            next(error)
+        }
+    },
+}
+export default controller
