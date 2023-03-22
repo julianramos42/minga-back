e0f5be5605405c91b161692d45c963a1f5ebf0d5
JuanBarberis <juanbarberis.97@gmail.com>
2023-03-22 10:15:17 -0300

    fix

--- test/T-01.test.js
+++ test/T-01.test.js
@@ -5,11 +5,12 @@ import request from 'supertest'
 const { expect, assert } = chai
 
 const data = {
-    title: 'Mis Mangas...',
+    title: 'Mis Mangas juan',
     cover_photo: 'https://example.com/image.jpg',
     description: 'Esta es la descripción de mi manga',
+    category_id:'641472da1214a15666e3ece9',
 }
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MTQ3MmRiMTIxNGExNTY2NmUzZWNlZSIsImlhdCI6MTY3OTQyMzU3OSwiZXhwIjoxNjc5NTA5OTc5fQ.5XfyDuL0mKscPkaJ4TFvVyEfdN7gy1IWOIUm4Mq4knM"
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MTQ3MmRiMTIxNGExNTY2NmUzZWNlZSIsImlhdCI6MTY3OTQ4NzU0OSwiZXhwIjoxNjc5NTczOTQ5fQ.DcXSj-GM4Smw6kxAIvoPQWy0ICmcloBJErrClocAQO0"
 
 
 describe("test 01 sobre/mangas", () => {
@@ -27,25 +28,25 @@ describe("test 01 sobre/mangas", () => {
             .auth(token, { type: "bearer" })
         expect(response.body).to.have.property("mangas")
     });
-    
+
     it('POST api/mangas Debería retornar un objeto con una URL válida en cover_photo', async () => {
         const res = await request(app)
-            .post('api/mangas/')
-            .set("Authorization", `Bearer ${token}`)
+            .post('/api/mangas/')
+            .auth(token, { type: "bearer" })
             .send(data);
 
-        expect(res.status).to.equal(201);
+        expect(res.status).to.equal(200);
         expect(res.body).to.be.an('object');
         expect(res.body.manga.cover_photo).to.match(/^https?:\/\/.+$/);
     });
 
     it("POST api/mangas verificar que la respuesta devuelve (no autorizado) cuando no se pasa token	", async () => {
         const res = await request(app)
-            .post('api/mangas/')
+            .post('/api/mangas/')
             .send(data)
 
         assert.equal(res.status, 401)
     });
 
-   
+
 })
