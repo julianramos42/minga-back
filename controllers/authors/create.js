268bd44c1b273e8c40a6879cb97caa4d2fcc0b21
Julián Ramos <99379303+julianramos42@users.noreply.github.com>
2023-03-28 17:58:35 -0300

    Dev extra (#43)
    * arreglos
    
    * fix
    
    * fix
    
    * arreglo controlador de reactions
    
    * tiempo de token extendido
    
    * ruta modificada
    
    * active en false por default

--- controllers/authors/create.js
+++ controllers/authors/create.js
@@ -5,12 +5,12 @@ const controller = {
         // req.body.user_id = "6400e1c1de3abf200587e13c"
         const { user } = req
         req.body.user_id = user._id
-        req.body.active = true
+        req.body.active = false
         try{
             await Author.create(req.body)
             return res.status(200).json({
                 success: true,
-                message: 'New author created succesfully',
+                message: 'New author created succesfully, please wait to be activated by an administrator',
                 data: req.body
             })
         }catch(error){
--- controllers/company/create.js
+++ controllers/company/create.js
@@ -4,12 +4,12 @@ const controller = {
     create: async (req, res, next) => {
         const { user } = req
         req.body.user_id = user._id
-        req.body.active = true
+        req.body.active = false
         try {
             await Company.create(req.body)
             return res.status(200).json({
                 success: true,
-                message: 'New company created succesfuly',
+                message: 'New company created succesfuly, please wait to be activated by an administrator',
                 data: req.body
             })
         } catch (error) {
